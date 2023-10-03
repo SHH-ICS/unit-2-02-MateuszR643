08a407e87e1405c7af95ce374a0f942bed8c0508
MateuszR643 <144241927+MateuszR643@users.noreply.github.com>
2023-10-03 09:29:01 -0400

    Update program.py

--- program.py
+++ program.py
@@ -2,5 +2,5 @@
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
 def myFunction(string name):
-   return "Hello" name "!" 
+   return "Hello" + name + "!" 
   # My Function
